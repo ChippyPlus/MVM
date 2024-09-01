417b4a5f146a9556e0ba53b58c1ee8a44c84c713
Adam Mathlay <mathlayadam@gmail.com>
2024-09-01 13:16:58 +0200

    Added DataTransfer.lit

---
+++ src/main/kotlin/kvmInternals/instructions/dataTransfer/lit.kt
@@ -0,0 +1,26 @@
+package org.example.kvmInternals.instructions.dataTransfer
+
+import org.example.data.registers.enumIdenifiers.SuperRegisterType
+import org.example.data.registers.enumIdenifiers.toGeneralRegisterType
+import org.example.data.registers.enumIdenifiers.toReturnRegisterType
+import org.example.data.registers.enumIdenifiers.toSystemRegisterType
+import org.example.generalRegisters
+import org.example.returnRegisters
+import org.example.systemRegisters
+
+fun DataTransfer.lit(Source: SuperRegisterType, value: Int) {
+    when (Source.toString()[0]) {
+        'G' -> {
+            generalRegisters.write(Source.toGeneralRegisterType(), value)
+        }
+
+        'S' -> {
+            systemRegisters.write(Source.toSystemRegisterType(), value)
+        }
+
+        'R' -> {
+            returnRegisters.write(Source.toReturnRegisterType(), value)
+        }
+    }
+
+}
