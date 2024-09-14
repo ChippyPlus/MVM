4f4f556086ded1b3455b6144124fadb0777efc47
Adam Mathlay <mathlayadam@gmail.com>
2024-09-14 16:53:22 +0200

    fixed

--- src/main/kotlin/internals/instructions/strings/strcpy.kt
+++ src/main/kotlin/internals/instructions/strings/strcpy.kt
@@ -1,28 +1,14 @@
 package internals.instructions.strings
 
 import org.example.data.memory.MemoryAddress
-import org.example.data.memory.MemoryValue
 import org.example.data.registers.enumIdenifiers.SuperRegisterType
-import org.example.errors
 import org.example.helpers.fullRegisterRead
 import org.example.helpers.readRegisterString
-import org.example.internalMemory
+import org.example.helpers.writeStringSpecInMemory
 import org.example.kvmInternals.instructions.strings.Strings
 
 fun Strings.strcpy(source: SuperRegisterType, destination: SuperRegisterType) {
     val string = readRegisterString(source)
     val destinationAddress = fullRegisterRead(destination)
-
-    val allocMem = string.length
-
-    for (i in (destinationAddress until (destinationAddress + allocMem))) {
-        if (internalMemory.memory[MemoryAddress(i)] != MemoryValue(null)) {
-            errors.NotFreeMemoryException(i.toString())
-        }
-    }
-
-    for ((index, i) in (destinationAddress until (destinationAddress + allocMem)).withIndex()) {
-        internalMemory.memory[MemoryAddress(i)] = MemoryValue(string[index].code.toLong())
-    }
-    internalMemory.memory[MemoryAddress(destinationAddress + allocMem)] = MemoryValue(0)
+    writeStringSpecInMemory(string, MemoryAddress(destinationAddress))
 }
