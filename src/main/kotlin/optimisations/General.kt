551c64afba8fc743165eca04d222cb44602a4800
Adam Mathlay <mathlayadam@gmail.com>
2024-10-02 19:22:22 +0200

    added findRedundancy

--- src/main/kotlin/optimisations/General.kt
+++ src/main/kotlin/optimisations/General.kt
@@ -4,16 +4,18 @@ import data.registers.enumIdenifiers.SuperRegisterType
 import engine.execution.InstructData
 
 
+typealias RegisterTree = MutableMap<SuperRegisterType, MutableList<RegisterUnderstanding>>
+
 enum class StatusType {
-    WRITTEN,
-    READ,
+    USE, DECLARED,
+
 }
 
 
-data class RegisterUnderstanding(val register: SuperRegisterType, var status: StatusType)
+data class RegisterUnderstanding(val register: SuperRegisterType, var status: StatusType, val lineNumber: Int)
 
 class General(/* val globalInfo: List<InstructData> */) {
-    fun findUsagesRegisters() {
+    fun parseRegisterStatuses(): MutableList<RegisterUnderstanding> {
         val tracked = mutableListOf<RegisterUnderstanding>()
         val test = listOf(
             InstructData(name = "lit", arrayOf(SuperRegisterType.G2, 10)),
@@ -25,22 +27,47 @@ class General(/* val globalInfo: List<InstructData> */) {
             InstructData(name = "prints", values = arrayOf())
         )
 
-        for (i in test) {
+        for ((index, i) in test.withIndex()) {
             if (i.name == "lit") {
-                tracked.add(RegisterUnderstanding(i.values[0] as SuperRegisterType, StatusType.WRITTEN))
+                tracked.add(RegisterUnderstanding(i.values[0] as SuperRegisterType, StatusType.DECLARED, index))
             } else {
                 for (j in i.values.withIndex()) {
                     if (j.value is SuperRegisterType) {
-                        tracked.add(RegisterUnderstanding(j.value as SuperRegisterType, StatusType.READ))
-
+                        tracked.add(RegisterUnderstanding(j.value as SuperRegisterType, StatusType.USE, index))
                     }
                 }
             }
         }
         tracked.forEach(::println)
+        return tracked
     }
+
+    fun findRedundancy(tracked: MutableList<RegisterUnderstanding>): RegisterTree {
+        val singleRegisterUses: RegisterTree =
+            emptyMap<SuperRegisterType, MutableList<RegisterUnderstanding>>().toMutableMap()
+
+
+        for (i in tracked) {
+            if (i.register !in singleRegisterUses.keys) {
+                singleRegisterUses[i.register] = mutableListOf()
+            }
+
+            singleRegisterUses[i.register]!!.add(i)
+
+        }
+//        for (i in singleRegisterUses) {
+//            println(i)
+//        }
+        return singleRegisterUses
+    }
+
+    fun removeRedundancy(singleRegisterUses: RegisterTree) {
+
+    }
+
 }
 
 fun main() {
-    General().findUsagesRegisters()
+    General().findRedundancy(General().parseRegisterStatuses())
+
 }
