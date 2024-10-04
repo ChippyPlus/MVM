536b18a070caa2ab638e62fe3c913cd78462efcf
Adam Mathlay <mathlayadam@gmail.com>
2024-10-04 18:21:56 +0200

    New constants

--- src/main/kotlin/internals/Vm.kt
+++ src/main/kotlin/internals/Vm.kt
@@ -27,3 +27,4 @@ open class Vm {
 
 
 
+
---
+++ src/main/kotlin/uniConstants.kt
@@ -0,0 +1,2 @@
+val r4Outs = setOf("add", "sub", "mul", "div", "mod", "strlen", "strcmp", "strcat")
+val r3Outs = setOf("and", "or", "not", "xor", "shr", "shl")
