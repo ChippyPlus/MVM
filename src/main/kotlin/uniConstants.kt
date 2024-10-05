0718252e0a25617945fb30647df562c101e9d136
Adam Mathlay <mathlayadam@gmail.com>
2024-10-05 20:46:59 +0200

    docs for readIO

--- src/main/kotlin/uniConstants.kt
+++ src/main/kotlin/uniConstants.kt
@@ -1,2 +1,2 @@
-val r4Outs = setOf("add", "sub", "mul", "div", "mod", "strlen", "strcmp", "strcat")
+val r4Outs = setOf("add", "sub", "mul", "div", "mod", "strlen", "strcmp", "strcat", "lt", "gt")
 val r3Outs = setOf("and", "or", "not", "xor", "shr", "shl")
