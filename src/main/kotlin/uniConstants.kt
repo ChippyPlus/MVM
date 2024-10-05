ae3965f47baca81a8870b601576f8078777be834
Adam Mathlay <mathlayadam@gmail.com>
2024-10-06 00:20:29 +0200

    start

--- src/main/kotlin/uniConstants.kt
+++ src/main/kotlin/uniConstants.kt
@@ -1,2 +1,12 @@
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+
 val r4Outs = setOf("add", "sub", "mul", "div", "mod", "strlen", "strcmp", "strcat", "lt", "gt")
 val r3Outs = setOf("and", "or", "not", "xor", "shr", "shl")
+
+@OptIn(ExperimentalSerializationApi::class)
+val json = Json {
+	prettyPrint = true
+	allowStructuredMapKeys = true
+	allowComments = true
+}
