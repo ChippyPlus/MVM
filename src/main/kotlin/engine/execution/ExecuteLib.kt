2c10b1c1ade61aa982b4a3c653a054535906e3ac
Adam Mathlay <mathlayadam@gmail.com>
2024-10-06 10:32:24 +0200

    added get MetaData

--- src/main/kotlin/engine/execution/ExecuteLib.kt
+++ src/main/kotlin/engine/execution/ExecuteLib.kt
@@ -1,15 +1,17 @@
 package engine.execution
 
+import helpers.LibMetaData
 import java.io.File
 
 class ExecuteLib(val name: String) {
 	private val f = File("./libs/$name.lib").readText()
 
-	private fun getMeta() {
+	private fun getMeta(): LibMetaData {
 		val data = f.split('\n')[0]
-		val name = data.split(' ')[0].removePrefix("!")
-		val args = data.substring(name.length).split(' ')
-		return
+		val name = data.split(':')[0].removePrefix("!")
+		val version = data.split(':')[1].removePrefix("!")
+		val args = data.substring(name.length + version.length).split(':')
+		return LibMetaData(name = name, version = version.toInt(), args = args)
 	}
 
 }
