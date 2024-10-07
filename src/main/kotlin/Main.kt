88ee2dba7701f7b489d95685c227f4478e985085
Adam Mathlay <mathlayadam@gmail.com>
2024-10-07 15:27:37 +0200

    pretty

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
-
 import data.io.FileDescriptors
 import data.memory.InternalMemory
 import data.registers.FunctionRegisters
@@ -32,85 +31,86 @@ val functionRegisters = FunctionRegisters()
 val generalRegisters = GeneralRegisters()
 val execute = Execute()
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("Usage: mvm <command> [options]")
-        exitProcess(1)
-    }
-    when (args[0]) {
-        "irun" -> {
-            if (args.size < 2) {
-                println("Usage: mvm irun <file.kar>")
-                exitProcess(1)
-            }
-            execute.execute(File(args[1]))
-        }
+	if (args.isEmpty()) {
+		println("Usage: mvm <command> [options]")
+		exitProcess(1)
+	}
+	when (args[0]) {
+		"irun" -> {
+			if (args.size < 2) {
+				println("Usage: mvm irun <file.kar>")
+				exitProcess(1)
+			}
+			execute.execute(File(args[1]))
+		}
 
-        "crun" -> {
-            if (args.size < 2) {
-                println("Usage: mvm crun <file.mar>")
-                exitProcess(1)
-            }
-            ExecutionV2().execute(File(args[1]).readText())
-        }
+		"crun" -> {
+			if (args.size < 2) {
+				println("Usage: mvm crun <file.mar>")
+				exitProcess(1)
+			}
+			ExecutionV2().execute(File(args[1]).readText())
+		}
 
-        "run" -> {
-            if (args.size < 2) {
-                println("Usage: mvm run <file.kar>")
-                exitProcess(1)
-            }
-            ExecutionV2().execute(Compile().execute(parser(File(args[1]).readLines())))
-        }
+		"run" -> {
+			if (args.size < 2) {
+				println("Usage: mvm run <file.kar>")
+				exitProcess(1)
+			}
+			ExecutionV2().execute(Compile().execute(parser(File(args[1]).readLines())))
+		}
 
-        "tokenise" -> {
-            if (args.size < 2) {
-                println("Usage: mvm tokenise <file.kar>")
-                exitProcess(1)
-            }
-            parser(File(args[1]).readLines()).forEach(::println)
-        }
+		"tokenise" -> {
+			if (args.size < 2) {
+				println("Usage: mvm tokenise <file.kar>")
+				exitProcess(1)
+			}
+			parser(File(args[1]).readLines()).forEach(::println)
+		}
 
 
-        "otokenise" -> {
-            if (args.size < 2) {
-                println("Usage: mvm tokenise <file.kar>")
-                exitProcess(1)
-            }
-            VarRedundancy(globalInfo = parser(File(args[1]).readLines())).cleanRedundancy().forEach(::println)
-        }
+		"otokenise" -> {
+			if (args.size < 2) {
+				println("Usage: mvm tokenise <file.kar>")
+				exitProcess(1)
+			}
+			VarRedundancy(globalInfo = parser(File(args[1]).readLines())).cleanRedundancy().forEach(::println)
+		}
 
-        "compile" -> {
-            if (args.size < 2) {
-                println("Usage: mvm compile <file.kar>")
-                exitProcess(1)
-            }
-            val optimised = VarRedundancy(globalInfo = parser(File(args[1]).readLines())).cleanRedundancy()
-            val out = Compile().execute(optimised)
-            val f = File(args[1].split(".")[0] + ".mar")
-            f.createNewFile()
-            f.writeText(out)
-            println("Compiled with 0 Issues!!!!!")
-        }
+		"compile" -> {
+			println("WARNING. The compiler is experimental!!!!")
+			if (args.size < 2) {
+				println("Usage: mvm compile <file.kar>")
+				exitProcess(1)
+			}
+			val optimised = VarRedundancy(globalInfo = parser(File(args[1]).readLines())).cleanRedundancy()
+			val out = Compile().execute(optimised)
+			val f = File(args[1].split(".")[0] + ".mar")
+			f.createNewFile()
+			f.writeText(out)
+			println("Compiled with 0 Issues!!!!!")
+		}
 
-        "debug" -> {// TODO Make this an option
-            if (args.size < 3) {
-                println("Usage: mvm debug <debugFile.json> <file.kar>")
-                exitProcess(1)
-            }
-            val debugEngine = DebugEngine(Json.decodeFromString<DebugFile>(File(args[1]).readText()))
-            execute.execute(File(args[2]), debugEngine)
-        }
+		"debug" -> {// TODO Make this an option
+			if (args.size < 3) {
+				println("Usage: mvm debug <debugFile.json> <file.kar>")
+				exitProcess(1)
+			}
+			val debugEngine = DebugEngine(Json.decodeFromString<DebugFile>(File(args[1]).readText()))
+			execute.execute(File(args[2]), debugEngine)
+		}
 
-        "help" -> {
-            println(
-                "mvm irun <file.kar> - Runs KAR code in interpreter mode with no optimisations, (This is the most stable mode)\n" + "mvm compile <file.kar> - Compiles and optimises the KAR code into the byte code stored in file.mar\n" + "mvm crun <file.mar> - Runs compiled code\n" + "mvm run <file.kar> - Compiles and runs code without creating a file\n" + "mvm tokenise <file.kar> - Shows the tokenised version of the code to the terminal\n" + "mvm otokenise <file.kar> - Shows the optimised tokenised version of the code to the terminal"
-            )
-        }
+		"help" -> {
+			println(
+				"mvm irun <file.kar> - Runs KAR code in interpreter mode with no optimisations, (This is the most stable mode)\n" + "mvm compile <file.kar> - Compiles and optimises the KAR code into the byte code stored in file.mar\n" + "mvm crun <file.mar> - Runs compiled code\n" + "mvm run <file.kar> - Compiles and runs code without creating a file\n" + "mvm tokenise <file.kar> - Shows the tokenised version of the code to the terminal\n" + "mvm otokenise <file.kar> - Shows the optimised tokenised version of the code to the terminal"
+			)
+		}
 
-        else -> {
-            println("Usage: mvm <command> <arguments...>")
-            exitProcess(1)
-        }
-    }
+		else -> {
+			println("Usage: mvm <command> <arguments...>")
+			exitProcess(1)
+		}
+	}
 }
 
 
