b153ecb481057d33b51144283760b02eae156e18
Adam Mathlay <mathlayadam@gmail.com>
2024-10-11 15:48:08 +0200

    fixed stringToSuperRegisterType.kt

--- src/main/kotlin/helpers/stringToSuperRegisterType.kt
+++ src/main/kotlin/helpers/stringToSuperRegisterType.kt
@@ -11,37 +11,66 @@ import kotlin.system.exitProcess
  * @return The corresponding [SuperRegisterType].
  * @throws InvalidRegisterException If the input string is not a valid register name.
  */
-fun String.toUnsafeSuperRegisterType(): SuperRegisterType {
-	return when (this.uppercase()) {
-		"G1" -> SuperRegisterType.G1
-		"G2" -> SuperRegisterType.G2
-		"G3" -> SuperRegisterType.G3
-		"G4" -> SuperRegisterType.G4
-		"S0" -> SuperRegisterType.S0
-		"S1" -> SuperRegisterType.S1
-		"S2" -> SuperRegisterType.S2
-		"S3" -> SuperRegisterType.S3
-		"R1" -> SuperRegisterType.R1
-		"R2" -> SuperRegisterType.R2
-		"R3" -> SuperRegisterType.R3
-		"R4" -> SuperRegisterType.R4
-		"F1" -> SuperRegisterType.F1
-		"F2" -> SuperRegisterType.F2
-		"F3" -> SuperRegisterType.F3
-		"F4" -> SuperRegisterType.F4
-		"IF1" -> SuperRegisterType.IF1
-		"IF2" -> SuperRegisterType.IF2
-		"IF3" -> SuperRegisterType.IF3
-		"IF4" -> SuperRegisterType.IF4
-		else -> error("bal")
+fun String.toUnsafeSuperRegisterType(): SuperRegisterType? {
+
+	return when (this.lowercase()) {
+		"g0" -> SuperRegisterType.G0
+		"g1" -> SuperRegisterType.G1
+		"g2" -> SuperRegisterType.G2
+		"g3" -> SuperRegisterType.G3
+		"g4" -> SuperRegisterType.G4
+		"g5" -> SuperRegisterType.G5
+		"g6" -> SuperRegisterType.G6
+		"g7" -> SuperRegisterType.G7
+		"g8" -> SuperRegisterType.G8
+		"g9" -> SuperRegisterType.G9
+
+		"s0" -> SuperRegisterType.S0
+		"s1" -> SuperRegisterType.S1
+		"s2" -> SuperRegisterType.S2
+		"s3" -> SuperRegisterType.S3
+
+		"r0" -> SuperRegisterType.R0
+		"r1" -> SuperRegisterType.R1
+		"r2" -> SuperRegisterType.R2
+		"r3" -> SuperRegisterType.R3
+		"r4" -> SuperRegisterType.R4
+		"r5" -> SuperRegisterType.R5
+		"r6" -> SuperRegisterType.R6
+		"r7" -> SuperRegisterType.R7
+		"r8" -> SuperRegisterType.R8
+		"r9" -> SuperRegisterType.R9
+
+		"f0" -> SuperRegisterType.F0
+		"f1" -> SuperRegisterType.F1
+		"f2" -> SuperRegisterType.F2
+		"f3" -> SuperRegisterType.F3
+		"f4" -> SuperRegisterType.F4
+		"f5" -> SuperRegisterType.F5
+		"f6" -> SuperRegisterType.F6
+		"f7" -> SuperRegisterType.F7
+		"f8" -> SuperRegisterType.F8
+		"f9" -> SuperRegisterType.F9
+
+		"if0" -> SuperRegisterType.IF0
+		"if1" -> SuperRegisterType.IF1
+		"if2" -> SuperRegisterType.IF2
+		"if3" -> SuperRegisterType.IF3
+		"if4" -> SuperRegisterType.IF4
+		"if5" -> SuperRegisterType.IF5
+		"if6" -> SuperRegisterType.IF6
+		"if7" -> SuperRegisterType.IF7
+		"if8" -> SuperRegisterType.IF8
+		"if9" -> SuperRegisterType.IF9
+		else -> null
 	}
 }
 
 fun String.toSuperRegisterType(): SuperRegisterType {
 	try {
-		return this.toUnsafeSuperRegisterType()
-	} catch (e: IllegalStateException) {
+		return this.toUnsafeSuperRegisterType()!!
+	} catch (e: NullPointerException) {
 		errors.InvalidRegisterException(this)
-		exitProcess(999)
+		exitProcess(320947)
 	}
 }
