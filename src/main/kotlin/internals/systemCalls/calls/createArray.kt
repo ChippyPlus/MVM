2195142b617b9ea06b1bedc4d5963f2970bb2948
Adam Mathlay <mathlayadam@gmail.com>
2024-10-11 16:35:47 +0200

    new count meta data

--- src/main/kotlin/internals/systemCalls/calls/createArray.kt
+++ src/main/kotlin/internals/systemCalls/calls/createArray.kt
@@ -10,9 +10,15 @@ import internalMemory
 import internals.systemCalls.SystemCall
 
 
+/**
+ * Format
+ * Array Max size | Current size | values...
+ */
 fun SystemCall.createArray(size: SuperRegisterType) {
 	val nSize = registerRead(size)
-	val spot = findFreeMemory(nSize + 1)
+	val spot = findFreeMemory(nSize + 2)
 	internalMemory.write(MemoryAddress(spot), MemoryValue(nSize))
+	internalMemory.write(MemoryAddress(spot + 1), MemoryValue(0))
+
 	registerWrite(SuperRegisterType.R2, spot)
 }
