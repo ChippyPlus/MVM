0aa2e4ac13254d30c3e300da729511d55f3cb335
Adam Mathlay <mathlayadam@gmail.com>
2024-10-11 15:22:47 +0200

    added new registers up to 10, except for S

--- src/main/kotlin/data/registers/FunctionRegisters.kt
+++ src/main/kotlin/data/registers/FunctionRegisters.kt
@@ -11,10 +11,16 @@ import kotlin.system.exitProcess
  * Function Registers registers are used as arguments in function calls that are user defined or builtin
  */
 class FunctionRegisters {
+	var f0: Long? = null
 	var f1: Long? = null
 	var f2: Long? = null
 	var f3: Long? = null
 	var f4: Long? = null
+	var f5: Long? = null
+	var f6: Long? = null
+	var f7: Long? = null
+	var f8: Long? = null
+	var f9: Long? = null
 
 	/**
 	 * Reads the value from the specified general-purpose register.
@@ -25,12 +31,7 @@ class FunctionRegisters {
 	 */
 	fun read(registers: FunctionRegisterType): Long {
 		try {
-			return when (registers) {
-				FunctionRegisterType.F1 -> f1!!
-				FunctionRegisterType.F2 -> f2!!
-				FunctionRegisterType.F3 -> f3!!
-				FunctionRegisterType.F4 -> f4!!
-			}
+			return readUnsafe(registers)!!
 		} catch (e: NullPointerException) {
 			errors.NullRegisterException(registers.toString().toSuperRegisterType())
 			exitProcess(11) // To satisfy the compiler. This shouldn't trigger
@@ -42,10 +43,16 @@ class FunctionRegisters {
 
 	fun readUnsafe(registers: FunctionRegisterType): Long? {
 		return when (registers) {
+			FunctionRegisterType.F0 -> f0
 			FunctionRegisterType.F1 -> f1
 			FunctionRegisterType.F2 -> f2
 			FunctionRegisterType.F3 -> f3
 			FunctionRegisterType.F4 -> f4
+			FunctionRegisterType.F5 -> f5
+			FunctionRegisterType.F6 -> f6
+			FunctionRegisterType.F7 -> f7
+			FunctionRegisterType.F8 -> f8
+			FunctionRegisterType.F9 -> f9
 		}
 	}
 
@@ -57,10 +64,16 @@ class FunctionRegisters {
 	 */
 	fun write(registers: FunctionRegisterType, value: Long?) {
 		when (registers) {
+			FunctionRegisterType.F0 -> f0 = value
 			FunctionRegisterType.F1 -> f1 = value
 			FunctionRegisterType.F2 -> f2 = value
 			FunctionRegisterType.F3 -> f3 = value
 			FunctionRegisterType.F4 -> f4 = value
+			FunctionRegisterType.F5 -> f5 = value
+			FunctionRegisterType.F6 -> f6 = value
+			FunctionRegisterType.F7 -> f7 = value
+			FunctionRegisterType.F8 -> f8 = value
+			FunctionRegisterType.F9 -> f9 = value
 		}
 	}
 }
