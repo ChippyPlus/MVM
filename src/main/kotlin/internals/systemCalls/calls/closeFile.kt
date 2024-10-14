c499c7d1d2bfb2677b70501534c8f1c73efed86d
Adam Mathlay <mathlayadam@gmail.com>
2024-10-14 14:17:34 +0200

    thing?

--- src/main/kotlin/internals/systemCalls/calls/closeFile.kt
+++ src/main/kotlin/internals/systemCalls/calls/closeFile.kt
@@ -1,5 +1,3 @@
-@file:Suppress("MoveLambdaOutsideParentheses")
-
 package internals.systemCalls.calls
 
 import data.registers.enumIdenifiers.SuperRegisterType
@@ -16,19 +14,19 @@ import internals.systemCalls.SystemCall
  * @param s2 The register containing the file descriptor to close (stored in register S1).
  */
 fun SystemCall.closeFile(s2: SuperRegisterType): Unit = try {
-    val fd: Long = registerRead(register = s2)
+	val fd: Long = registerRead(register = s2)
 
-    if (fileDescriptors.fds.remove(fd)?.equals(null)
-            ?: throw NullPointerException("Expression 'fileDescriptors.fds.remove(fd)?.equals(null)' must not be null")
-    ) {
-        errors.run {
-            this@run.InvalidFileDescriptorException(message = with(fd) { return@with this.toString() })
-        }
-    } else {/* pass */
+	if (fileDescriptors.fds.remove(fd)?.equals(null)
+			?: throw NullPointerException("Expression 'fileDescriptors.fds.remove(fd)?.equals(null)' must not be null")
+	) {
+		errors.run {
+			this@run.InvalidFileDescriptorException(message = with(fd) { return@with this.toString() })
+		}
+	} else {/* pass */
 
-    }
+	}
 } catch (e: Exception) {
-    with(receiver = errors, block = {
-        this@with.SystemCallGeneralException(message = "closeFile")
-    })
+	with(receiver = errors, block = {
+		this@with.SystemCallGeneralException(message = "closeFile")
+	})
 }
