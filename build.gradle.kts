c01cfd9120eb368718277bf49c3834af794ae833
Adam Mathlay <mathlayadam@gmail.com>
2024-10-14 06:50:53 +0200

    upgrade to kotlin  2.1.0-Beta1. COOL STRINGS!!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.1.0-Beta1"
     kotlin("plugin.serialization") version "2.0.20"
     application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
 
 kotlin {
     jvmToolchain(17)
