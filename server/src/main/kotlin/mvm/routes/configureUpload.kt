71223e515e9ce388a43d825f715d3c9798099f19
Adam Mathlay <mathlayadam@gmail.com>
2024-10-21 19:07:01 +0200

    fixed no duplicates in upload

--- server/src/main/kotlin/mvm/routes/configureUpload.kt
+++ server/src/main/kotlin/mvm/routes/configureUpload.kt
@@ -4,6 +4,16 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestPackage(
+	val repository: String,
+	val module: String,
+	val `package`: String,
+	val name: String,
+	val version: String,
+)
 
 fun Application.configureUpload() {
 	routing {
@@ -13,9 +23,24 @@ fun Application.configureUpload() {
 			try {
 				Upload().fullAddPackage(body)
 				call.respondText("SUCCESS")
+			} catch (e: IllegalAccessError) {
+				call.respondText("already exists error")
 			} catch (e: Exception) {
 				call.respond("ERROR")
 			}
 		}
+
+		get("/api/v1/repo/download") {
+			val body = call.receive<RequestPackage>()
+
+			try {
+				val o = Upload().download(body)
+				call.respond(o!!)
+			} catch (e: NullPointerException) {
+				call.respond("ERROR: Not found")
+			}
+
+		}
+
 	}
 }
--- server/src/main/kotlin/mvm/routes/upload.kt
+++ server/src/main/kotlin/mvm/routes/upload.kt
@@ -2,6 +2,7 @@ package mvm.routes
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import mvm.PackageFormat
 import mvm.RepositoryFormat
 import java.io.File
 
@@ -41,9 +42,52 @@ fun Upload.fullAddPackage(packClass: RequestFormat) {
 	if (moduleIndex == null) return
 
 	val packageNeighbours = x[repositoryIndex].modules[moduleIndex].packages.toMutableList()
+
+
+	val tmp = mutableListOf<String>()
+	tmp.add(packClass.`package`.name + ":" + packClass.`package`.version)
+	println(packageNeighbours.map { "${it.name}:${it.version}" })
+	for (i in packageNeighbours) {
+
+		if ("${i.name}:${i.version}" in tmp) throw IllegalAccessError("Package already exists")
+		if ("${i.name}:${i.version}" !in tmp) tmp.add("${i.name}:${i.version}")
+		println("${i.name}:${i.version} --- ${tmp}")
+
+	}
+
 	packageNeighbours.add(packClass.`package`)
 	val copy = x
 	copy[repositoryIndex].modules[moduleIndex].packages = packageNeighbours
 	f.writeText(json.encodeToString<List<RepositoryFormat>>(copy))
-
 }
+
+
+fun Upload.download(info: RequestPackage): PackageFormat? {
+	var repositoryIndex: Int? = null
+	var moduleIndex: Int? = null
+
+	for (i in x.withIndex()) {
+		if (i.value.repository == info.repository) {
+			repositoryIndex = i.index
+			break
+		}
+	}
+	if (repositoryIndex == null) return null
+
+	for (i in x[repositoryIndex].modules.withIndex()) {
+		if (i.value.module == info.module) {
+			moduleIndex = i.index
+			break
+		}
+	}
+	if (moduleIndex == null) return null
+
+	val packageNeighbours = x[repositoryIndex].modules[moduleIndex].packages.toMutableList()
+
+	if ("${info.name}:${info.version}" !in packageNeighbours.map { "${info.name}:${info.version}" }) {
+		throw IllegalAccessError("Package does not exist")
+	}
+
+	return x[repositoryIndex].modules[moduleIndex].packages[packageNeighbours.indexOfFirst { "${info.name}:${info.version}" == "${it.name}:${it.version}" }]
+}
+
