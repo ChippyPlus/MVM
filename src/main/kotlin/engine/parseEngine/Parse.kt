8211ff5b2a780ba5ac61ecb36f9fe8a92b52efdb
Adam Mathlay <mathlayadam@gmail.com>
2024-11-01 18:48:59 +0200

    result tokenizing!

--- src/main/kotlin/engine/parseEngine/Parse.kt
+++ src/main/kotlin/engine/parseEngine/Parse.kt
@@ -1,10 +1,36 @@
 package engine.parseEngine
 
+import data.registers.RegisterType
 import java.io.File
 
 
 @Suppress("ArrayInDataClass")
-data class TokenData(val name: String, val type: InstructType, val values: Array<Any>)
+data class TokenData(
+	val name: String,
+	val type: InstructType,
+	val values: Array<Any>,
+	val result: String? = calcResult(name, values),
+)
+
+private fun calcResult(name: String, values: Array<Any>): String? {
+	return when (name) {
+		"add", "sub", "mul", "div", "mod", "pow" -> "R4"
+		"xadd", "xsub", "xmul", "xdiv", "xmod", "xpow" -> "R5"
+		"shl", "shr", "and", "or", "xor", "not" -> "R3"
+		"gt", "lt" -> "I3"
+		"EQ" -> "I4"
+		"load" -> (values[1] as RegisterType).toString()
+		"store" -> (values[0] as RegisterType).toString()
+		"pop" -> (values[0] as RegisterType).toString()
+		"peek" -> (values[0] as RegisterType).toString()
+		"str" -> (values[0] as RegisterType).toString()
+		"inr" -> "R6"
+		"printr" -> "IO"
+		"lit" -> (values[0] as RegisterType).toString()
+		else -> null
+	}
+}
+
 
 enum class InstructType {
 	Ignore, Register, RegisterRegister, RegisterLong, Long, LongRegister, SingleString, StrSpecific, None, Settype, Xlit, SystemCall
--- src/main/kotlin/optimisations/TestOp.kt
+++ src/main/kotlin/optimisations/TestOp.kt
@@ -1,9 +1,6 @@
 package optimisations
 
-import data.registers.RegisterType
-import engine.parseEngine.InstructType
 import engine.parseEngine.Parse
-import engine.parseEngine.TokenData
 import java.io.File
 import kotlin.math.pow
 
@@ -25,46 +22,6 @@ val f = Parse(File("main.mar")).tokenise()
 fun main() {
 	val o = Optimisations(f)
 	val inp = o.insertValuesLit().toMutableList()
-
-
-	fun updateNextR4(startIndex: Int) {
-		for ((index, content) in inp.toMutableList().subList(startIndex, inp.size).withIndex()) {
-			if (content.name in arrayOf("add", "sub", "mul", "div", "mod", "pow") || (content.name in arrayOf(
-					"lit", "mov", "cpy", "load", "printr"
-				) && RegisterType.R4 in content.values)
-			) {
-				println("hit")
-				for (j in content.values.withIndex()) {
-					if (j.value is RegisterType && j.value == RegisterType.R4) {
-						content.values[j.index] = RegisterType.R3
-						inp[index] = content
-					}
-				}
-			}
-		}
-	}
-
-
-
-	for (i in inp.toMutableList().withIndex()) {
-		if (i.value.name == "mul") {
-			if (checkBase2(i.value.values[0] as Long) != null) {
-				inp.removeAt(i.index)
-				inp.add(
-					i.index,
-					TokenData("shl", InstructType.RegisterRegister, arrayOf(i.value.values[1], i.value.values[0]))
-				)
-				updateNextR4(i.index)
-			} else if (checkBase2(i.value.values[1] as Long) != null) {
-				inp.removeAt(i.index)
-				inp.add(
-					i.index,
-					TokenData("shl", InstructType.RegisterRegister, arrayOf(i.value.values[0], i.value.values[1]))
-				)
-				updateNextR4(i.index)
-			}
-		}
-	}
 
 	inp.forEach(::println)
 }
