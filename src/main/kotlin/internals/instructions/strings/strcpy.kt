df328607cb607ac3b64c1dcf99ae16e4b198e48b
Adam Mathlay <mathlayadam@gmail.com>
2024-11-13 19:17:25 +0200

    removed docs

--- src/main/kotlin/internals/instructions/strings/strcpy.kt
+++ src/main/kotlin/internals/instructions/strings/strcpy.kt
@@ -8,14 +8,6 @@ import helpers.readRegisterString
 import helpers.toLong
 import helpers.writeStringSpecInMemory
 
-/**
- * Copies a string from one memory location to another.
- *
- * @param source The register containing the memory address of the source string.
- * @param destination The register containing the memory address of the destination location.
- * @throws GeneralStringException If an error occurs during the string copy.
- */
-
 @Deprecated("Moved into stdlib functions")
 fun Strings.strcpy(source: RegisterType, destination: RegisterType): Unit = try {
 	registers.write(intelNames[IntelRegisters.ENSF], true.toLong())
