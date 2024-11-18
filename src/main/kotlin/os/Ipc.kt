3ce88bb8808c8888b9008bdd2239455fa3a1d764
Adam Mathlay <mathlayadam@gmail.com>
2024-11-18 18:25:50 +0200

    start?

---
+++ src/main/kotlin/os/Ipc.kt
@@ -0,0 +1,20 @@
+package os
+
+
+/**
+ * # Goals.
+ * ## Syscalls
+ * - Receive,
+ * - Send,
+ * - Listen,
+ */
+
+
+class Ipc {
+
+	inner class SharedMemory
+
+	inner class MessagePassing
+
+
+}
---
+++ src/main/kotlin/os/KProcess.kt
@@ -0,0 +1,5 @@
+package os
+
+import internals.Vm
+
+data class KProcess(val id: Int, val vm: Vm)
