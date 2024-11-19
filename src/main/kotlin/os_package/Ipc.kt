fb414a591e649af928431c9651348f325e1f9ad2
Adam Mathlay <mathlayadam@gmail.com>
2024-11-19 10:06:07 +0200

    new link system

--- src/main/kotlin/os_package/Ipc.kt
+++ src/main/kotlin/os_package/Ipc.kt
@@ -40,27 +40,47 @@ class Ipc {
 //		val name: String = "${max(p1.id, p2.id)}-${min(p1.id, p2.id)}",
 		)
 
+		val mailBoxes = mutableMapOf<Pair<Int, Int>, RelationShip>()
+		val pendingLinks = mutableListOf<Pair<Int, Int>>()
 
-		fun arrangeKProcess(p1: KProcess, p2: KProcess) = "${max(p1.id, p2.id)}-${min(p1.id, p2.id)}"
 
-		val mailBox = mutableMapOf<String, RelationShip>()
+		fun arrangeKProcess(p1: KProcess, p2: KProcess) = Pair(max(p1.id, p2.id), (min(p1.id, p2.id)))
 
 		val listener = null // !!! Not Implemented !!!
 
+		fun isLinked(p1: KProcess, p2: KProcess): Boolean {
+			val relationShipRequest = Pair(max(p1.id, p2.id), (min(p1.id, p2.id)))
+			if (relationShipRequest in pendingLinks) return false
+			if (relationShipRequest !in mailBoxes) return false
+			return true
+		}
+
+
+		fun preposeLink(p1: KProcess, p2: KProcess): Boolean {
+			val relationShipRequest = Pair(max(p1.id, p2.id), (min(p1.id, p2.id)))
+			if (relationShipRequest in pendingLinks) {
+				pendingLinks.remove(relationShipRequest)
+				return true
+			}
+			pendingLinks.add(relationShipRequest)
+			return false
+		}
+
+
 		fun link(p1: KProcess, p2: KProcess) {
 			val relationShip = RelationShip(p1, p2, IpcType.MailBox)
-			mailBox[arrangeKProcess(p1, p2)] = relationShip
+			mailBoxes[arrangeKProcess(p1, p2)] = relationShip
 		}
 
 		fun send(senderVm: KProcess, receiverVm: KProcess, message: Long): Unit? {
 			val key = arrangeKProcess(senderVm, receiverVm)
-			mailBox[key]?.mailbox?.push(message) ?: return null
+			mailBoxes[key]?.mailbox?.push(message) ?: return null
 			return Unit
 		}
 
 		fun revive(senderVm: KProcess, receiverVm: KProcess): Long? {
 			val key = arrangeKProcess(senderVm, receiverVm)
-			return mailBox[key]?.mailbox?.pop()
+			return mailBoxes[key]?.mailbox?.pop()
 		}
 	}
 
