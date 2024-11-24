39b709adc80bfbd7df3732e1a18e4314d5faa3d2
Adam Mathlay <mathlayadam@gmail.com>
2024-11-24 22:16:53 +0200

    new file sharing

--- archive.sh
+++ archive.sh
@@ -1,2 +1,34 @@
-#!/usr/bin/env bash
-zip -r archive.zip src docs/wiki build.gradle.kts examples
+#!/bin/zsh
+
+rm -rf .archiveBuffer
+git clone --depth 1 https://github.com/ChippyPlus/micro-vm.git .archiveBuffer
+
+find .archiveBuffer > .archiveOutBuffer
+
+files=""
+
+while IFS= read -r i; do
+    i=${i%$'\n'}
+
+    if [[ -d "$i" || "$i" =~ ^.archiveBuffer/gradle ]]; then
+        continue
+    fi
+
+    ext=""
+    case "$i" in
+        *.kt) ext="kotlin" ;;
+        *.md) ext="markdown" ;;
+        *.sh) ext="shell" ;;
+        *.json) ext="json" ;;
+        *.kar) ext="custom assembly" ;;
+    esac
+
+    echo $i
+    if [[ -n "$ext" ]]; then
+        files+=$'\nfilePath '$i$'\n```'$ext$'\n'$(cat "$i")$'\n-------------------------------\n```'
+    fi
+done <  .archiveOutBuffer
+
+
+
+echo "$files" > share.txt
