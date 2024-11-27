867b44ebba1192cb745a69d3878e22d1149b4b8f
Adam Mathlay <122267295+ChippyPlus@users.noreply.github.com>
2024-11-27 15:55:57 +0200

    Update xSub.kt

--- src/main/kotlin/internals/instructions/xFloats/xSub.kt
+++ src/main/kotlin/internals/instructions/xFloats/xSub.kt
@@ -3,7 +3,7 @@ package internals.instructions.xFloats
 import data.registers.FDRegister
 import data.registers.RegisterType
 import registers
-
+ 
 
 fun XFloats.xSub(operand1: RegisterType, operand2: RegisterType) {
 	val o1 = registers.readX(operand1)
