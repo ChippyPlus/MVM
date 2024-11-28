c4f869fda20332e48c9c654931bba9ca2c78ecf6
Adam Mathlay <mathlayadam@gmail.com>
2024-11-28 13:21:39 +0200

    yes

--- src/main/kotlin/data/vfs/Vfs.kt
+++ src/main/kotlin/data/vfs/Vfs.kt
@@ -17,17 +17,17 @@ class Vfs {
 	init {
 		if (!File("src/main/resources/vfs.fs").exists()) {
 			File("src/main/resources/vfs.fs").createNewFile()
-			this.flash(listOf(Ventry("/", null, Permissions(directory = true))))
+			this.flash(listOf(Ventry("/", "", Permissions(directory = true))))
 
 		}
 	}
 
-	fun error(message: String) {
+	fun error(message: String): Nothing {
 		System.err.println(message)
 		exitProcess(1)
 	}
 
-	fun mk(p: Ventry, mode: Mode = Mode.Create) {
+	private fun mk(p: Ventry, mode: Mode = Mode.Create) {
 		if (p.path.endsWith('/')) {
 			p.permissions.directory = true
 			p.path = p.path.removeSuffix("/")
@@ -43,20 +43,26 @@ class Vfs {
 
 		println(pathParts)
 		var memoized = ""
+
+		if (p !in ren && mode == Mode.Remove) {
+			error("Ventry not found")
+		}
+
 		for (i in pathParts.withIndex()) {
 			memoized += "/${pathParts[i.index]}"
 			println(memoized)
 
-
 			if (i.index == pathParts.size - 1) {
 				if (memoized in ren.map(Ventry::path)) {
+					if (mode == Mode.Remove) {
+						flash(ren - p)
+						return
+					}
 					error("Ventry already exists")
 				} else {
 					if (mode == Mode.Create) flash(ren + p)
-					else if (mode == Mode.Remove) flash(ren - p)
 					return
 				}
-
 			}
 
 			if (memoized !in ren.map(Ventry::path)) {
@@ -66,19 +72,50 @@ class Vfs {
 
 	}
 
-	fun rm(p: Ventry) = mk(p, Mode.Remove)
+	private fun rm(p: Ventry) = mk(p, Mode.Remove)
+	fun read(path: String): String {
+		val rend = list()
+		for (ventry in rend) {
+			if (ventry.path == path) {
+				return ventry.content
+			}
+		}
+		error("Ventry Not found")
+	}
+
+	fun write(path: String, content: String) {
+		val rend = list()
+		for (ventry in rend.withIndex()) {
+			if (ventry.value.path == path) {
+				ventry.value.content = content
+				println(ventry.value === rend[ventry.index])
+			}
+		}
+		println(rend)
+	}
 
 	fun flash(ventries: List<Ventry>) =
 		File("src/main/resources/vfs.fs").writeText(ProtoBuf.encodeToByteArray(value = ventries).toHexString())
 
 	fun list(): List<Ventry> =
 		ProtoBuf.decodeFromHexString<List<Ventry>>(hex = File("src/main/resources/vfs.fs").readText())
+
+
+	fun mkFile(path: String) {
+		mk(Ventry(path = path, content = "", permissions = Permissions(directory = false, write = true, read = true)))
+	}
+
+	fun rmFile(path: String) {
+		rm(Ventry(path = path, content = "", permissions = Permissions(directory = false, write = true, read = true)))
+	}
+
+
 }
 
 
 fun main() {
 	val v = Vfs()
-	v.rm(Ventry(path = "/sounds/cats/meow.txt", content = "Meow!", Permissions(directory = false, write = false)))
+	v.rmFile("/sounds")
 	v.list().forEach(::println)
 }
 
