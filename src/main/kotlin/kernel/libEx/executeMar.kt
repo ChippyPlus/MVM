aae7b7d607bf84eff0058993e8b26459abdb652b
Adam Mathlay <mathlayadam@gmail.com>
2024-11-30 13:18:10 +0200

    fixed with instruct memory

--- src/main/kotlin/kernel/libEx/executeMar.kt
+++ src/main/kotlin/kernel/libEx/executeMar.kt
@@ -2,19 +2,18 @@ package kernel.libEx
 
 import engine.execution.Execute
 import engine.parserReturn
-import environment.reflection.reflection
 import kernel.ExecuteLib
 import java.io.File
 
 fun ExecuteLib.executeMar(file: File) {
 	val vm = vm
-	val oldPc = vm.pc
+	val oldPC = kp.vm.pc
+	val oldInstructs = kp.instructionMemory
 	val snapshot = vm.snapShotManager.snapShotRegisters()
 	vm.pc - 2
-
 	val instructions = parserReturn(vm, file.readLines())
-
-	Execute(reflection.groupTrackedVmByVm()[vm]!!).run(instructions)
+	Execute(kp).run(instructions)
 	vm.snapShotManager.populateSnapShotRegister(snapshot)
-	vm.pc = oldPc
+	kp.vm.pc = oldPC
+	kp.instructionMemory = oldInstructs
 }
